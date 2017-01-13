0a9985fa7ab1d885ccb90312af56dab7b69185ea
Park <june3377@nate.com>
2017-01-13 14:11:25 +0900

    int example

---
+++ VariableExample/src/com/variavle_example/VariavleExam.java
@@ -0,0 +1,14 @@
+package com.variavle_example;
+
+public class VariavleExam {
+
+	public static void main(String[] args) {
+		
+		int value = 10;
+		int result = value+10;
+		
+		System.out.println(result);
+		
+	}
+
+}
